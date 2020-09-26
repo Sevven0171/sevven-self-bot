7d13b2909ca45abd7ec934135934c1d30f87b19c
s3vv3n <69166649+s3vv3n@users.noreply.github.com>
2020-09-26 12:50:14 -0300

    Create abc.js

---
+++ comandos/abc.js
@@ -0,0 +1,27 @@
+const request = require("request-promise");
+
+module.exports = {
+    "info": {
+        "permission": 1,
+        "name": "abc",
+        "description": "Send the entire alphabet by editing the message",
+        "aliases": [],
+        "syntax": "abc"
+    },
+    run: (bot, msg, args) => {
+        msg.delete().catch(() => {});
+        
+        // dont start with A because we send A as initial message
+        let alphabet = ["B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+        let x = 0;
+
+        msg.channel.send("A").then(message => {
+            let alphabetInterval = setInterval(() => {
+                message.edit(alphabet[x]);
+                x++;
+
+                if(x == alphabet.length) clearInterval(alphabetInterval);
+            }, 1000);
+        });
+    }
+}
