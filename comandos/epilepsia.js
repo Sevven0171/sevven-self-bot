1e9f2dc3f95d2f9f56b13db973b45a5a3c4d0b31
s3vv3n <69166649+s3vv3n@users.noreply.github.com>
2020-09-26 12:53:11 -0300

    Create epilepsia.js

---
+++ comandos/epilepsia.js
@@ -0,0 +1,16 @@
+const request = require("request-promise");
+
+module.exports = {
+    "info": {
+        "permission": 1,
+        "name": "epilepsy",
+        "description": "Sends an epileptic gif",
+        "aliases": ["potato"],
+        "syntax": "epilepsy"
+    },
+    run: (bot, msg, args) => {
+        msg.delete().catch(() => {});
+
+        msg.channel.send({files: [{"attachment": "https://i.imgur.com/aj4WtET.gif", "name": `epilepsy.gif`}]});
+    }
+}
